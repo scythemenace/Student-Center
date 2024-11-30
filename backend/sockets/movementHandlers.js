20b8527be8decc8c2992f2e7372bbe5a79acab4d
Ankur Pandey <pankur216@gmail.com>
2024-11-30 11:02:47 -0500

    Removed direction emit

--- backend/sockets/movementHandlers.js
+++ backend/sockets/movementHandlers.js
@@ -3,14 +3,12 @@ module.exports = (socket, io, users) => {
     // Update user's position
     users[socket.id].x = data.x;
     users[socket.id].y = data.y;
-    users[socket.id].direction = data.direction;
 
     // Broadcast movement to others
     socket.broadcast.emit("userMoved", {
       userId: socket.id,
       x: data.x,
       y: data.y,
-      direction: data.direction,
     });
   });
 };
