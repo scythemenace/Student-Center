6dd56643edb1d0941675d33f0f4ddafb2b603a19
Ankur Pandey <pankur216@gmail.com>
2024-11-30 23:22:05 -0500

    Emitting YOURID as well in order to identify peer-to-peer connections

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ require("dotenv").config();
 
 // Use CORS
 app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Replace with your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
+	cors({
+		origin: process.env.FRONTEND_URL || "https://localhost:5173", // Replace with your frontend URL
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
 );
 
 app.use(express.json());
@@ -22,10 +22,10 @@ app.use("/api/v1", rootRouter);
 
 const server = http.createServer(app);
 const io = socketIo(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Ensure the frontend URL is allowed
-    methods: ["GET", "POST"],
-  },
+	cors: {
+		origin: process.env.FRONTEND_URL || "http://localhost:5173", // Ensure the frontend URL is allowed
+		methods: ["GET", "POST"],
+	},
 });
 
 setupSocket(io);
@@ -33,8 +33,8 @@ setupSocket(io);
 const port = process.env.PORT || 3000;
 
 server.listen(port, (err) => {
-  if (err) console.log(err);
-  console.log("Server listening on PORT", port);
+	if (err) console.log(err);
+	console.log("Server listening on PORT", port);
 });
 
 module.exports = app;
