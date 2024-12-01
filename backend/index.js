f75a39bb3a887a96ab995eb28d3d4eca3d4cd969
Ankur Pandey <pankur216@gmail.com>
2024-12-01 01:59:39 -0500

    reverted

--- backend/index.js
+++ backend/index.js
@@ -8,25 +8,24 @@ require("dotenv").config();
 
 // Use CORS
 app.use(
-    cors({
-      origin: process.env.FRONTEND_URL  "https://your-frontend-domain.com/",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+	cors({
+		origin: process.env.FRONTEND_URL || "https://your-frontend-domain.com/",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
+	})
+);
 
 app.use(express.json());
 
 const rootRouter = require("./routes/index");
 app.use("/api/v1", rootRouter);
 
-
 const server = http.createServer(app);
 const io = socketIo(server, {
-    cors: {
-        origin: process.env.FRONTEND_URL  "http://localhost:5173/", // Ensure the frontend URL is allowed
-        methods: ["GET", "POST"],
-    },
+	cors: {
+		origin: process.env.FRONTEND_URL || "http://localhost:5173/", // Ensure the frontend URL is allowed
+		methods: ["GET", "POST"],
+	},
 });
 
 setupSocket(io);
@@ -34,9 +33,8 @@ setupSocket(io);
 const port = process.env.PORT || 3000;
 
 server.listen(port, (err) => {
-  if (err) console.log(err);
-  console.log("Server listening on PORT", port);
+	if (err) console.log(err);
+	console.log("Server listening on PORT", port);
 });
-
 
 module.exports = app;
